d06050abed3808d325ec6a6ebff79baac0cff8bc
forhadhossen1 <mr.forhad1030@gamil.com>
2023-12-22 17:05:24 +0600

    added deploy json file

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const userCollection = client.db("taskDB").collection('users');
         const taskCollection = client.db("taskDB").collection('allTask');
